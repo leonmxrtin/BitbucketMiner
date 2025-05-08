3a22260398affd9820e127a896ce4b0b719731f9
León Martín <leonmartinl@icloud.com>
2025-05-08 20:27:57 +0200

    add logic for fetching commits, place url in config files

---
+++ src/main/java/aiss/bitbucketminer/service/CommitService.java
@@ -0,0 +1,37 @@
+package aiss.bitbucketminer.service;
+
+import aiss.bitbucketminer.model.Commit;
+import aiss.bitbucketminer.model.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CommitService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public List<Commit> getCommits(String repoUri, Integer pageLen, Integer maxPages) {
+        List<Commit> commits = new ArrayList<>();
+
+        // Fetch data from the API until limits are satisfied
+        for (int page = 1; page < maxPages; page++) {
+            String commitUri = repoUri + "/commits" + "?pagelen=" + pageLen + "&page=" + page;
+            Page<Commit> fetchedPage = restTemplate.exchange(commitUri, HttpMethod.GET, null,
+                    new ParameterizedTypeReference<Page<Commit>>(){}).getBody();
+
+            if (fetchedPage != null) {
+                List<Commit> fetchedCommits = fetchedPage.getValues();
+                commits.addAll(fetchedCommits);
+            }
+        }
+
+        return commits;
+    }
+}
--- src/main/java/aiss/bitbucketminer/service/ProjectService.java
+++ src/main/java/aiss/bitbucketminer/service/ProjectService.java
@@ -1,21 +1,14 @@
 package aiss.bitbucketminer.service;
 
-import aiss.bitbucketminer.controller.ProjectController;
 import aiss.bitbucketminer.exception.ProjectNotFoundException;
-import aiss.bitbucketminer.model.Commit;
-import aiss.bitbucketminer.model.Issue;
 import aiss.bitbucketminer.model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static java.util.Objects.isNull;
 
 @Service
 public class ProjectService {
@@ -23,28 +16,29 @@ public class ProjectService {
     @Autowired
     RestTemplate restTemplate;
 
-    private final String baseUri = "https://api.bitbucket.org";
-    private final String apiVersion = "/2.0";
+    @Autowired
+    CommitService commitService;
 
-    private final String gitMinerUri = "http://localhost:8080/gitminer/projects";
+    @Value("${bitbucket.uri}")
+    private String bitbucketUri;
+
+    @Value("${gitminer.uri}")
+    private String gitminerUri;
 
     // TODO: add commit and issue logic
     public Project getProject(String workspace, String repoSlug, Integer nCommits, Integer nIssues, Integer maxPages)
             throws ProjectNotFoundException {
-        String uri = baseUri + apiVersion + "/repositories/" + workspace + "/" + repoSlug + "?pageLen=" + maxPages;
-        ResponseEntity<Project> response = restTemplate.getForEntity(uri, Project.class);
+        String repoUri = bitbucketUri + "/repositories/" + workspace + "/" + repoSlug;
+        ResponseEntity<Project> response = restTemplate.getForEntity(repoUri, Project.class);
 
         if (response.getStatusCode() != HttpStatus.OK || response.getBody() == null) {
             throw new ProjectNotFoundException();
         }
 
         Project project = response.getBody();
-//        Commit[] commits = restTemplate.getForObject(uri + "/commits", Commit[].class);
+        project.setCommits(commitService.getCommits(repoUri, nCommits, maxPages));
+
 //        Issue[] issues = restTemplate.getForObject(uri + "/issues", Issue[].class);
-//        if (!isNull(commits)) {
-//            commits = Arrays.stream(commits).toList().limit(nCommits);
-//            project.setCommits(commits);
-//        }
 //        if (!isNull(issues)) {
 //            issues = Arrays.stream(issues).toList().limit(nIssues);
 //            project.setIssues(issues);
@@ -56,6 +50,6 @@ public class ProjectService {
     public Project createProject(String workspace, String repoSlug, Integer nCommits, Integer nIssues, Integer maxPages)
             throws ProjectNotFoundException {
         Project project = getProject(workspace, repoSlug, nCommits, nIssues, maxPages);
-        return restTemplate.postForObject(gitMinerUri, project, Project.class);
+        return restTemplate.postForObject(gitminerUri + "/projects", project, Project.class);
     }
 }
