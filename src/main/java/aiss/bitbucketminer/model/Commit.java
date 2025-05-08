fc7f39438e461bbce4094066f1a97634f745a57d
León Martín <leonmartinl@icloud.com>
2025-05-08 20:23:02 +0200

    add commit model

--- src/main/java/aiss/bitbucketminer/model/Commit.java
+++ src/main/java/aiss/bitbucketminer/model/Commit.java
@@ -1,4 +1,152 @@
 package aiss.bitbucketminer.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Commit {
+
+    @JsonProperty("id")
+    @JsonAlias("hash")
+    private String id;
+
+    @JsonProperty("title")
+    private String title;
+
+    private String message;
+    @JsonProperty("message")
+    private void parseMessage(String message) {
+        this.message = this.title = message;
+
+        // if multi-line message, set title to its first line
+        if (message.lines().count() > 1) {
+            this.title = message.lines().findFirst().get();
+        }
+    }
+
+    @JsonProperty("author_name")
+    private String authorName;
+
+    @JsonProperty("author_email")
+    private String authorEmail;
+
+    @JsonProperty("author")
+    private void unpackAuthor(JsonNode author) {
+        this.authorName = author.get("user").get("display_name").asText();
+
+        Matcher emailRegex = Pattern.compile(".* <(.*)>").matcher(author.get("raw").asText());
+        if (emailRegex.find()) {
+            this.authorEmail = emailRegex.group(1);
+        }
+    }
+
+    @JsonAlias("date")
+    @JsonProperty("authored_date")
+    private String authoredDate;
+
+    @JsonProperty("web_url")
+    private String webUrl;
+    @JsonProperty("links")
+    private void unpackWebUrl(JsonNode links) {
+        this.webUrl = links.get("html").get("href").asText();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorEmail() {
+        return authorEmail;
+    }
+
+    public void setAuthorEmail(String authorEmail) {
+        this.authorEmail = authorEmail;
+    }
+
+    public String getAuthoredDate() {
+        return authoredDate;
+    }
+
+    public void setAuthoredDate(String authoredDate) {
+        this.authoredDate = authoredDate;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Commit.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null) ? "<null>" : this.id));
+        sb.append(',');
+        sb.append("title");
+        sb.append('=');
+        sb.append(((this.title == null) ? "<null>" : this.title));
+        sb.append(',');
+        sb.append("message");
+        sb.append('=');
+        sb.append(((this.message == null) ? "<null>" : this.message));
+        sb.append(',');
+        sb.append("authorName");
+        sb.append('=');
+        sb.append(((this.authorName == null) ? "<null>" : this.authorName));
+        sb.append(',');
+        sb.append("authorEmail");
+        sb.append('=');
+        sb.append(((this.authorEmail == null) ? "<null>" : this.authorEmail));
+        sb.append(',');
+        sb.append("authoredDate");
+        sb.append('=');
+        sb.append(((this.authoredDate == null) ? "<null>" : this.authoredDate));
+        sb.append(',');
+        sb.append("webUrl");
+        sb.append('=');
+        sb.append(((this.webUrl == null) ? "<null>" : this.webUrl));
+        sb.append(',');
+        if (sb.charAt((sb.length() - 1)) == ',') {
+            sb.setCharAt((sb.length() - 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
 }
