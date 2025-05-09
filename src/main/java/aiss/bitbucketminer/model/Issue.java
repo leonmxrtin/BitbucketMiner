f70e4bbfcf02ce3133693f28b15f7c4cf7f7ce2f
León Martín <leonmartinl@icloud.com>
2025-05-09 16:37:04 +0200

    refactor issue model

--- src/main/java/aiss/bitbucketminer/model/Issue.java
+++ src/main/java/aiss/bitbucketminer/model/Issue.java
@@ -1,7 +1,9 @@
 package aiss.bitbucketminer.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,56 +12,57 @@ import java.util.List;
 
 public class Issue {
 
-    @JsonProperty("uuid")
-    private String id;
-    @JsonProperty("title")
-    private String title;
-    private String refId;
-    private String description;
-    private String state;
-    private String createdAt;
-    private String updatedAt;
-    private String closedAt;
-    private Integer upvotes;
-    private Integer downvotes;
-    private String webUrl;
-    private User author;
-    private User assignee;
-
-    private List<Comment> comments;
-    private List<String> labels;
-
     public Issue() {
-        comments = new ArrayList<>();
         labels = new ArrayList<>();
     }
 
-    @JsonProperty("uuid")
+    private String id;
+    private String title;
+    private String state;
+    private Integer votes;
+    private Project project;
+    private User author;
+    private User assignee;
+    private List<Comment> comments;
+
+    private String description;
+    @JsonProperty("content")
+    private void unpackDescription(JsonNode content) {
+        this.description = content.get("raw").asText();
+    }
+
+    private List<String> labels;
+    @JsonProperty("kind")
+    private void parseKind(String kind) {
+        labels.add(kind);
+    }
+    @JsonProperty("priority")
+    private void parsePriority(String priority) {
+        labels.add(priority);
+    }
+
+    @JsonProperty("created_at")
+    @JsonAlias("created_on")
+    private String createdAt;
+
+    @JsonProperty("updated_at")
+    @JsonAlias("updated_on")
+    private String updatedAt;
+
     public String getId() {
         return id;
     }
 
-    @JsonProperty("uuid")
     public void setId(String id) {
         this.id = id;
     }
 
-    @JsonProperty("title")
     public String getTitle() {
         return title;
     }
 
-    @JsonProperty("title")
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public String getRefId() {
-        return refId;
-    }
-
-    public void setRefId(String refId) {
-        this.refId = refId;
     }
 
     public String getDescription() {
@@ -94,12 +97,9 @@ public class Issue {
         this.updatedAt = updatedAt;
     }
 
+    @JsonProperty("closed_at")
     public String getClosedAt() {
-        return closedAt;
-    }
-
-    public void setClosedAt(String closedAt) {
-        this.closedAt = closedAt;
+        return getState().equals("closed") ? getUpdatedAt() : null;
     }
 
     public List<String> getLabels() {
@@ -108,6 +108,22 @@ public class Issue {
 
     public void setLabels(List<String> labels) {
         this.labels = labels;
+    }
+
+    public Integer getVotes() {
+        return votes;
+    }
+
+    public void setVotes(Integer votes) {
+        this.votes = votes;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
     }
 
     public User getAuthor() {
@@ -126,104 +142,11 @@ public class Issue {
         this.assignee = assignee;
     }
 
-    public Integer getUpvotes() {
-        return upvotes;
-    }
-
-    public void setUpvotes(Integer upvotes) {
-        this.upvotes = upvotes;
-    }
-
-    public Integer getDownvotes() {
-        return downvotes;
-    }
-
-    public void setDownvotes(Integer downvotes) {
-        this.downvotes = downvotes;
-    }
-
-    public String getWebUrl() {
-        return webUrl;
-    }
-
-    public void setWebUrl(String webUrl) {
-        this.webUrl = webUrl;
-    }
-
     public List<Comment> getComments() {
         return comments;
     }
 
     public void setComments(List<Comment> comments) {
         this.comments = comments;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(Issue.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
-        sb.append("id");
-        sb.append('=');
-        sb.append(((this.id == null) ? "<null>" : this.id));
-        sb.append(',');
-        sb.append("refId");
-        sb.append('=');
-        sb.append(((this.refId == null) ? "<null>" : this.refId));
-        sb.append(',');
-        sb.append("title");
-        sb.append('=');
-        sb.append(((this.title == null) ? "<null>" : this.title));
-        sb.append(',');
-        sb.append("description");
-        sb.append('=');
-        sb.append(((this.description == null) ? "<null>" : this.description));
-        sb.append(',');
-        sb.append("state");
-        sb.append('=');
-        sb.append(((this.state == null) ? "<null>" : this.state));
-        sb.append(',');
-        sb.append("createdAt");
-        sb.append('=');
-        sb.append(((this.createdAt == null) ? "<null>" : this.createdAt));
-        sb.append(',');
-        sb.append("updatedAt");
-        sb.append('=');
-        sb.append(((this.updatedAt == null) ? "<null>" : this.updatedAt));
-        sb.append(',');
-        sb.append("closedAt");
-        sb.append('=');
-        sb.append(((this.closedAt == null) ? "<null>" : this.closedAt));
-        sb.append(',');
-        sb.append("labels");
-        sb.append('=');
-        sb.append(((this.labels == null) ? "<null>" : this.labels));
-        sb.append(',');
-        sb.append("author");
-        sb.append('=');
-        sb.append(((this.author == null) ? "<null>" : this.author));
-        sb.append(',');
-        sb.append("assignee");
-        sb.append('=');
-        sb.append(((this.assignee == null) ? "<null>" : this.assignee));
-        sb.append(',');
-        sb.append("upvotes");
-        sb.append('=');
-        sb.append(((this.upvotes == null) ? "<null>" : this.upvotes));
-        sb.append(',');
-        sb.append("downvotes");
-        sb.append('=');
-        sb.append(((this.downvotes == null) ? "<null>" : this.downvotes));
-        sb.append(',');
-        sb.append("comments");
-        sb.append('=');
-        sb.append(((this.comments == null) ? "<null>" : this.comments));
-        sb.append(',');
-
-        if (sb.charAt((sb.length() - 1)) == ',') {
-            sb.setCharAt((sb.length() - 1), ']');
-        } else {
-            sb.append(']');
-        }
-        return sb.toString();
     }
 }
