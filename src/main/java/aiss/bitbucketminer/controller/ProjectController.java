796ef1512ed617ae394ec00c6fa5acdc6d8f99ed
“alvaro <alvarova605@gmail.com>
2025-05-11 13:40:17 +0200

    added API annotations to ProjectController

--- src/main/java/aiss/bitbucketminer/controller/ProjectController.java
+++ src/main/java/aiss/bitbucketminer/controller/ProjectController.java
@@ -4,6 +4,11 @@ import aiss.bitbucketminer.exception.PageNotFoundException;
 import aiss.bitbucketminer.exception.ProjectNotFoundException;
 import aiss.bitbucketminer.model.Project;
 import aiss.bitbucketminer.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +19,14 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
+    @Operation(summary = "Retrieve a project",
+            description = "Get a project given its workspace and slug",
+            tags = {"projects", "get"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    content = {@Content(schema = @Schema(implementation = Project.class), mediaType = "application/json")})
+    })
     @GetMapping("/{workspace}/{repo_slug}")
     public Project getProject(@PathVariable String workspace,
                               @PathVariable String repo_slug,
@@ -24,6 +37,14 @@ public class ProjectController {
         return projectService.getProject(workspace, repo_slug, nCommits, nIssues, maxPages);
     }
 
+    @Operation(summary = "Retrieve and store a project",
+            description = "Get a project given its workspace and slug and send it to GitMiner",
+            tags = {"projects", "post"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    content = {@Content(schema = @Schema(implementation = Project.class), mediaType = "application/json")})
+    })
     @PostMapping("/{workspace}/{repo_slug}")
     public Project createProject(@PathVariable String workspace,
                                  @PathVariable String repo_slug,
