ca3bdda5496111be7b97de54324332161a08129e
ivamnnn <ivamunpri@alum.us.es>
2025-05-11 20:05:07 +0200

    Commit, comment and issue services tests

---
+++ src/main/java/aiss/bitbucketminer/exception/IssueNotFoundException.java
@@ -0,0 +1,7 @@
+package aiss.bitbucketminer.exception;
+
+public class IssueNotFoundException extends RuntimeException {
+  public IssueNotFoundException(String message) {
+    super(message);
+  }
+}
